ee18d167ebcfa93125462425a2fa651e7c4bdb5b
Ali B <abittin@gmail.com>
2016-07-29 21:35:10 +0300

    op3: bootanim already defined in vendor
    Change-Id: Iab67e73f5b42b0b3ad1aff5de6f3a0e69f1f7563

--- device.mk
+++ device.mk
@@ -66,10 +66,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=256m \
